f67bcc3f3d4b7ddd8e03734e9dd7c270f32a158e
Marcus Guedes <mclguedes@gmail.com>
2026-08-12 08:10:23 -0300

    Replace Personagens component with Welcome component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Personagens } from "../welcome/personagens";
+import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,5 +12,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Personagens />;
+  return <Welcome />;
 }
